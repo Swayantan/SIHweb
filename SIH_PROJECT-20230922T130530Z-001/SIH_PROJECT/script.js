6c6e22bca73bf6159d6ed179881243ae27ce0db5
Swayantan <swayantanmaji815@gmail.com>
2023-09-22 19:04:28 +0530

    2nd Commit with backend

---
+++ SIH_PROJECT-20230922T130530Z-001/SIH_PROJECT/script.js
@@ -0,0 +1,62 @@
+function generateCertificate() {
+    const name = document.getElementById("name").value;
+    const coursename = document.getElementById("coursename").value;
+    const courseid = document.getElementById("courseid").value;
+    const instname = document.getElementById("instname").value;
+    const startdate = document.getElementById("startdate").value;
+    const enddate = document.getElementById("enddate").value;
+
+    if (!name) {
+        alert("Please enter a name.");
+        return;
+    }
+
+    const certificateCanvas = document.getElementById("certificateCanvas");
+    const certificateContext = certificateCanvas.getContext("2d");
+
+    // Here we load the certificate template as an image
+    const templateImage = new Image();
+    templateImage.src = "certificate_template.png"; // Use certificate template image here
+
+    templateImage.onload = function () {
+        // Here we set canvas dimensions to match the template image
+        certificateCanvas.width = templateImage.width;
+        certificateCanvas.height = templateImage.height;
+
+        // Here we draw the template image on the canvas
+        certificateContext.drawImage(templateImage, 0, 0);
+
+        // Here we customize the certificate by adding the user's name
+        certificateContext.font = "bold 80px Arial";
+        certificateContext.fillStyle = "white";
+        certificateContext.fillText(name, 190, 480); // Adjust position here
+
+        // Here we customize the certificate by adding the course name
+        certificateContext.font = "bold 50px Arial";
+        certificateContext.fillText(coursename, 210, 665);
+
+        // Here we customize the certificate by adding the course id
+        certificateContext.font = "bold 50px Arial";
+        certificateContext.fillText(courseid, 550, 734);
+
+        // Here we customize the certificate by adding the institution name and location
+        certificateContext.font = "bold 50px Arial";
+        certificateContext.fillText(instname, 210, 860);
+
+        // Here we customize the certificate by adding the course start date
+        certificateContext.font = "bold 50px Arial";
+        certificateContext.fillText(startdate, 400, 935);
+
+        // Here we customize the certificate by adding the course end date
+        certificateContext.font = "bold 50px Arial";
+        certificateContext.fillText(enddate, 850, 935);
+
+        // Here we convert canvas to data URL (PNG)
+        const certificateDataURL = certificateCanvas.toDataURL("image/png");
+
+        // Here we set the download link href
+        const certificateLink = document.getElementById("downloadLink");
+        certificateLink.href = certificateDataURL;
+        certificateLink.style.display = "block";
+    };
+}
